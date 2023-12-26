eceb0c113ee9fd0331a154468c1616274af88056
camaradafurry <154755225+camaradafurry@users.noreply.github.com>
2023-12-25 21:49:31 -0300

    Create estatisticas.js

---
+++ estatisticas.js
@@ -0,0 +1,44 @@
+// Substitua 'YOUR_GOOGLE_SHEETS_URL' pela URL da sua planilha do Google Sheets
+const publicSpreadsheetUrl = 'YOUR_GOOGLE_SHEETS_URL';
+
+function init() {
+    Tabletop.init({
+        key: publicSpreadsheetUrl,
+        callback: showInfo,
+        simpleSheet: true
+    });
+}
+
+function showInfo(data, tabletop) {
+    // Exemplo de dados no console
+    console.log(data);
+
+    // Processar os dados conforme necessário para criar os gráficos
+    const labels = data.map(entry => entry.Label);
+    const values = data.map(entry => entry.Value);
+
+    // Configuração do gráfico
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Valores',
+                data: values,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+window.addEventListener('DOMContentLoaded', init);
